6575397edd760f772e08b25455c3c4f3f6d5910b
unknown <rohan2402>
2017-08-24 03:03:29 +0530

    complex multiplication

---
+++ complex_no.py
@@ -0,0 +1,7 @@
+#to multiply 2 complex numbers
+a=int(input("real part of 1st no="))
+b=int(input("complex part of 1st no="))
+c=int(input("real part of 2nd no="))
+d=int(input("complex part of 2nd no="))
+#complex multiplication
+print("after multiplication the complex no is",a*c,"+",b*d,"j")
